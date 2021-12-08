b58006dba5c8de96fa4eb6764f27b6db28d3cea3
PawelWisz <30932978+PawelWisz@users.noreply.github.com>
2021-12-08 21:34:26 +0100

    Lab 3

---
+++ main.cpp
@@ -0,0 +1,380 @@
+/***************************************************
+Code written for the optimization exercises purposes
+by Lukasz Sztangret, PhD
+Department of Applied Computer Science and Modelling
+AGH University of Science and Technology
+***************************************************/
+
+#include <iostream>
+#include <random>
+#include"opt_alg.h"
+#include"solution.h"
+
+
+struct Comma final : std::numpunct<char>
+{
+	char do_decimal_point() const override { return ','; }
+};
+
+int main()
+{
+	try
+	{
+		cout << "LAB NUMBER " << LAB_NO << endl;
+		cout << "LAB PART " << LAB_PART << endl << endl;
+#if LAB_NO==0
+
+#elif LAB_NO==1 && LAB_PART==1
+		std::uniform_real_distribution<double> unif(-100, 100);
+		std::default_random_engine re;
+		double alphas[3] = { 2, 5, 10};
+
+		for (int i = 0; i < 3; i++)
+		{
+			double alpha = alphas[i];
+			double output[100][12];
+			
+			for (int j = 0; j < 100; j++)
+			{
+				double x0 = unif(re);
+
+				double d = 100, epsilon = 1e-5, gamma = 1e-200; // gamma = d - d_old w lagrange
+				int N_max = 1000;
+
+				solution::clear_calls();
+				double* p = expansion(x0, d, alpha, N_max);
+
+				solution::clear_calls();
+				solution opt_fib = fib(p[0], p[1], epsilon);
+				int opt_fib_f_calls = solution::f_calls;
+
+				solution::clear_calls();
+				solution opt_lag = lag(p[0], p[1], epsilon, gamma, N_max);
+				int opt_lag_f_calls = solution::f_calls;
+
+				output[j][0] = x0;
+				output[j][1] = p[0];
+				output[j][2] = p[1];
+				output[j][3] = p[2];
+				output[j][4] = opt_fib.x[0]();
+				output[j][5] = opt_fib.y[0]();
+				output[j][6] = opt_fib_f_calls;
+				output[j][7] = opt_fib.x[0]() >= 62 && opt_fib.x[0]() <= 63 ? 1.0 : 0.0;
+				output[j][8] = opt_lag.x[0]();
+				output[j][9] = opt_lag.y[0]();
+				output[j][10] = opt_lag_f_calls;
+				output[j][11] = opt_lag.x[0]() >= 62 && opt_lag.x[0]() <= 63 ? 1.0 : 0.0;;
+			}
+
+			std::ofstream out(std::to_string(alpha) + "_lab_1_part_1.csv");
+
+			for (auto& row : output) {
+				for (auto col : row)
+					out << col << ',';
+				out << '\n';
+			}
+		}
+		
+
+		/*
+		double output[100][12];
+		
+		double x0 = 62, d = 70, alpha = 0.5, epsilon = 1e-5, gamma = 1e-200; // gamma = d - d_old w lagrange
+		int N_max = 1000;
+		double *p = expansion(x0, d, alpha, N_max);
+
+		std::cout << "p[0]: " << p[0] << std::endl;
+		std::cout << "p[1]: " <<  p[1] << std::endl;
+		std::cout << "p[2]: " <<  p[2] << std::endl;
+
+		solution::clear_calls();
+		solution opt_fib = fib(p[0], p[1], epsilon);
+		int opt_fib_f_calls = solution::f_calls;
+		std::cout << opt_fib << std::endl;
+
+		solution::clear_calls();
+		solution opt_lag = lag(p[0], p[1], epsilon, gamma, N_max);
+		int opt_lag_f_calls = solution::f_calls;
+		std::cout << opt_lag << std::endl;
+
+
+		output[0][0] = x0;
+		output[0][1] = p[0];
+		output[0][2] = p[1];
+		output[0][3] = p[2];
+		output[0][4] = opt_fib.x[0]();
+		output[0][5] = opt_fib.y[0]();
+		output[0][6] = opt_fib_f_calls;
+		output[0][7] = 1.0;
+		output[0][8] = opt_lag.x[0]();
+		output[0][9] = opt_lag.y[0]();
+		output[0][10] = opt_lag_f_calls;
+		output[0][11] = 1.0;
+
+		std::ofstream out(std::to_string(alpha) + "_lab_1_part_1.csv");
+
+		for (auto& row : output) {
+			for (auto col : row)
+				out << col << ',';
+			out << '\n';
+		}
+		*/
+		
+#elif LAB_NO==1 && LAB_PART==2
+		double epsilon = 1e-5, gamma = 1e-200;
+		int N_max = 1000;
+
+		matrix ab_F(1, 1, 200);
+		solution::clear_calls();
+		solution opt_f = fib(-100, 100, epsilon, &ab_F);
+		std::cout << opt_f << std::endl;
+		std::cout << solution::f_calls << std::endl;
+		std::cout << ab_F << std::endl;
+		
+		matrix ab_L(1, 1, 200);
+		solution::clear_calls();
+		solution opt_l = lag(-100, 100, epsilon, gamma, N_max, &ab_L);
+		std::cout << opt_l << std::endl;
+		std::cout << ab_L << std::endl;
+
+#elif LAB_NO==1 && LAB_PART==3
+		double epsilon = 1e-5, gamma = 1e-200;
+		int N_max = 1000;
+		
+		int row = 1001;
+		int col = 3;
+
+		matrix ab_F(row, col);
+		solution::clear_calls();
+		solution opt_f = fib(0.0001, 0.01, epsilon, &ab_F);
+		std::cout << opt_f << std::endl;
+		
+		std::ofstream out_f("lab_1_part_3_fib.csv");
+
+		for (int i = 0; i < row; ++i)
+		{
+			for (int j = 0; j < col; ++j)
+				if (j < (col - 1)) {
+					out_f << ab_F(i, j) << ",";
+				}
+				else if (j == (col - 1)) {
+					out_f << ab_F(i, j) << "\n";
+				}
+		}
+
+		matrix ab_L(row, col);
+		solution::clear_calls();
+		solution opt_l = lag(0.0001, 0.01, epsilon, gamma, N_max, &ab_L);
+		std::cout << opt_l << std::endl;
+
+		std::ofstream out_l("lab_1_part_3_lag.csv");
+
+		for (int i = 0; i < row; ++i)
+		{
+			for (int j = 0; j < col; ++j)
+				if (j < (col - 1)) {
+					out_l << ab_L(i, j) << ",";
+				}
+				else if (j == (col - 1)) {
+					out_l << ab_L(i, j) << "\n";
+				}
+		}
+		
+#elif LAB_NO==2 && LAB_PART==1
+		double epsilon = 1e-3, alpha_HJ = 0.5, aplha_R = 2, beta = 0.5;
+		int Nmax = 5000;
+
+		double steps[3] = { 0.1, 0.5, 1.0 };
+		auto isGlobalMin = [epsilon](double x1, double x2) {
+			x1 = abs(x1);
+			x2 = abs(x2);
+			return (x1 >= 0 && x1 < epsilon) && (x2 >= 0 && x2 < epsilon);
+		};
+
+		for (int i = 0; i < 3; i++)
+		{
+			double s = steps[i];
+			matrix s0 = matrix(2, 1, s);
+			double output[100][12];
+
+			for (int j = 0; j < 100; j++)
+			{
+				matrix x0 = 2 * rand_mat(2, 1) - 1;
+
+				solution::clear_calls();
+				solution opt_HJ = HJ(x0, s, alpha_HJ, epsilon, Nmax);
+				int opt_hj_f_calls = solution::f_calls;
+
+				solution::clear_calls();
+				solution opt_R = Rosen(x0, s0, aplha_R, beta, epsilon, Nmax);
+				int opt_rosen_f_calls = solution::f_calls;
+
+				output[j][0] = x0(0);
+				output[j][1] = x0(1);
+				output[j][2] = opt_HJ.x(0);
+				output[j][3] = opt_HJ.x(1);
+				output[j][4] = opt_HJ.y(0);
+				output[j][5] = opt_hj_f_calls;
+				output[j][6] = isGlobalMin(opt_HJ.x(0), opt_HJ.x(1)) ? 1.0 : 0;
+				output[j][7] = opt_R.x(0);
+				output[j][8] = opt_R.x(1);
+				output[j][9] = opt_R.y(0);
+				output[j][10] = opt_rosen_f_calls;
+				output[j][11] = isGlobalMin(opt_R.x(0), opt_R.x(1)) ? 1.0 : 0;
+			}
+
+			std::ofstream out(std::to_string(s) + "_lab_2_part_1.csv");
+
+			for (auto& row : output) {
+				for (auto col : row)
+					out << col << ',';
+				out << '\n';
+			}
+		}
+		
+#elif LAB_NO==2 && LAB_PART==2
+		double s = 0.1, epsilon = 1e-3, alpha_HJ = 0.5, aplha_R = 2, beta = 0.5;
+		int Nmax = 5000;
+		matrix s0 = matrix(2, 1, s);
+		matrix x0 = matrix(2, 1);
+		x0(0) = -0.0432063;
+		x0(1) = -0.343887;
+		cout << x0 << endl << endl;
+		
+		solution::clear_calls();
+		matrix ud_HJ(1, 2);
+		solution opt_HJ = HJ(x0, s, alpha_HJ, epsilon, Nmax, &ud_HJ);
+		cout << opt_HJ << endl << endl;
+		cout << ud_HJ << endl << endl;
+		
+		solution::clear_calls();
+		matrix ud_R(1, 2);
+		solution opt_R = Rosen(x0, s0, aplha_R, beta, epsilon, Nmax, &ud_R);
+		cout << opt_R << endl;
+		cout << ud_R << endl;
+
+#elif LAB_NO==2 && LAB_PART==3
+		double s = 0.1, epsilon = 1e-3, alpha_HJ = 0.5, aplha_R = 2, beta = 0.5;
+		int Nmax = 5000;
+		int row = 1001;
+		int col = 2;
+		matrix s0 = matrix(2, 1, s);
+		matrix x0 = matrix(2, new double[2]{ 1, 1 });
+		cout << x0 << endl << endl;
+
+		solution::clear_calls();
+		matrix ud_HJ(row, col);
+		solution opt_HJ = HJ(x0, s, alpha_HJ, epsilon, Nmax, &ud_HJ);
+		cout << opt_HJ << endl << endl;
+
+		std::ofstream out_hj("lab_2_part_3_hj.csv");
+
+		for (int i = 0; i < row; ++i)
+		{
+			for (int j = 0; j < col; ++j)
+				if (j < (col - 1)) {
+					out_hj << ud_HJ(i, j) << ",";
+				}
+				else if (j == (col - 1)) {
+					out_hj << ud_HJ(i, j) << "\n";
+				}
+		}
+
+		solution::clear_calls();
+		matrix ud_R(row, col);
+		solution opt_R = Rosen(x0, s0, aplha_R, beta, epsilon, Nmax, &ud_R);
+		cout << opt_R << endl;
+
+		std::ofstream out_r("lab_2_part_3_r.csv");
+
+		for (int i = 0; i < row; ++i)
+		{
+			for (int j = 0; j < col; ++j)
+				if (j < (col - 1)) {
+					out_r << ud_R(i, j) << ",";
+				}
+				else if (j == (col - 1)) {
+					out_r << ud_R(i, j) << "\n";
+				}
+		}
+#elif LAB_NO==3 && LAB_PART==1
+
+std::ofstream pen_zew;
+pen_zew.imbue(std::locale(std::locale::classic(), new Comma));
+std::ofstream pen_wew;
+pen_wew.imbue(std::locale(std::locale::classic(), new Comma));
+pen_zew.open("Lab03_P01_F_zew.csv");
+pen_wew.open("Lab03_P01_F_wew.csv");
+
+ for (int i = 0; i < 100; i++) {
+	matrix x0, a = 4; // a znajduje siê w g3
+
+
+	// zrobiæ losowanie tak, ¿eby by³o w obaszarze dopuszczalnym
+	do {
+		x0 = 4 * rand_mat(2, 1) + 1;
+	} while (norm(x0) > a(0));
+
+	pen_zew << x0(0) << ";" << x0(1);
+
+	//cout << x0 << endl << endl;
+
+
+	// zewnetrzna
+	double c0 = 1, dc = 2, epsilon = 1e-5;
+
+	int Nmax = 10000;
+
+	// 4,49
+
+	solution opt_zew = pen(x0, c0, dc, epsilon, Nmax, &a);
+	double result = sqrt(pow(opt_zew.x(0), 2) + pow(opt_zew.x(1), 2));
+	cout << opt_zew << endl << endl;
+	//cout << sqrt(pow(opt_zew.x(0), 2) + pow(opt_zew.x(1), 2))  << endl << endl; // 4.000001
+	cout << result << endl;
+
+	pen_zew << ";" << opt_zew.x(0) << ";" << opt_zew.x(1) << ";" << result << ";" << opt_zew.y(0) << ";" << opt_zew.f_calls << "\n";
+
+
+	solution::clear_calls();
+
+
+	// wewnetrzna
+	c0 = 10;
+	dc = 0.5;
+
+	solution opt_wew = pen(x0, c0, dc, epsilon, Nmax, &a);
+	cout << opt_wew << endl << endl;
+	//cout << sqrt(pow(opt_wew.x(0), 2) + pow(opt_zew.x(1), 2)) << endl << endl; // 4.000001
+	result = sqrt(pow(opt_wew.x(0), 2) + pow(opt_wew.x(1), 2));
+	cout << result << endl;
+
+	pen_wew << ";" << opt_wew.x(0) << ";" << opt_wew.x(1) << ";" << result << ";" << opt_wew.y(0) << ";" << opt_wew.f_calls << "\n";
+
+	solution::clear_calls();
+}
+
+pen_zew.close();
+pen_wew.close();
+
+#elif LAB_NO==3 && LAB_PART==2
+
+#elif LAB_NO==4 && LAB_PART==1
+
+#elif LAB_NO==4 && LAB_PART==2
+
+#elif LAB_NO==4 && LAB_PART==3
+
+#elif LAB_NO==5 && LAB_PART==1
+
+#elif LAB_NO==5 && LAB_PART==2
+
+#endif
+	}
+	catch (char * EX_INFO)
+	{
+		cout << EX_INFO << endl;
+	}
+	system("pause");
+	return 0;
+}
---
+++ solution.cpp
@@ -0,0 +1,178 @@
+//Do not edit the code below (unless you know what you are doing)
+
+#include"solution.h"
+
+int solution::f_calls = 0;
+int solution::g_calls = 0;
+int solution::H_calls = 0;
+
+solution::solution(double L)
+{
+	x = L;
+	g = NAN;
+	H = NAN;
+	y = NAN;
+}
+
+solution::solution(const matrix &A)
+{
+	x = A;
+	g = NAN;
+	H = NAN;
+	y = NAN;
+}
+
+solution::solution(int n, double *A)
+{
+	x = matrix(n, A);
+	g = NAN;
+	H = NAN;
+	y = NAN;
+}
+
+int get_dim(const solution &A)
+{
+	return get_len(A.x);
+}
+
+void solution::clear_calls()
+{
+	f_calls = 0;
+	g_calls = 0;
+	H_calls = 0;
+}
+
+ostream &operator<<(ostream &S, const solution &A)
+{
+	S << "x = " << A.x << endl;
+	S << "y = " << A.y << endl;
+	S << "f_calls = " << solution::f_calls << endl;
+	if (solution::g_calls>0)
+		S << "g_calls = " << solution::g_calls << endl;
+	if (solution::H_calls)
+		S << "H_calls = " << solution::H_calls << endl;
+	return S;
+}
+
+//You can edit the following code
+
+void solution::fit_fun(matrix *ud, matrix *ad)
+{
+	++f_calls;
+#if LAB_NO==1 && (LAB_PART==1 || LAB_PART==2)
+	y = -cos(0.1 * x()) * exp(-pow(0.1 * x() - 2 * 3.14, 2)) + 0.002 * pow(0.1 * x(), 2);
+#elif LAB_NO==1 && LAB_PART==3
+	matrix Y0 = matrix(3, new double[3]{5, 1, 10}); // Va, Vb, Tb
+	matrix *Y = solve_ode(0, 1, 1000, Y0, ud, &x); // od 0, krok 1, 1000 sekund symulacja
+	// Y[0] wketor czasu, Y[1], 3 kolumny Va, Vb, Tb, 1001 wpisów
+	int n = get_len(Y[0]);
+	double max = Y[1](0, 2);
+
+	for (int i = 1; i < n; i++)
+	{		
+		if (max < Y[1](i, 2)) {
+			max = Y[1](i, 2);
+		}
+
+		if (abs(x() - 0.00241025) < 1e-5)
+		{
+			matrix b(1, 3);
+			b(0, 0) = Y[1](i, 0);
+			b(0, 1) = Y[1](i, 1);
+			b(0, 2) = Y[1](i, 2);
+			(*ud).set_row(b, i);
+		}
+	}
+	y = abs(max - 50);
+#elif LAB_NO==2 && (LAB_PART==1 || LAB_PART==2)
+	y = pow(x(0), 2) + pow(x(1), 2) - cos(2.5 * 3.14 * x(0)) - cos(2.5 * 3.14 * x(1)) + 2;
+#elif LAB_NO==2 && LAB_PART==3
+	double a_ref = 3.14, w_ref = 0;
+	matrix Y0(2, 1);
+	matrix* Y = solve_ode(0, 0.1, 100, Y0, ud, &x);
+	int n = get_len(Y[0]);
+	y = 0;
+
+	for (int i = 0; i < n; i++)
+	{
+		y = y + 10 * pow(a_ref - Y[1](i, 0), 2) + pow(w_ref - Y[1](i, 1), 2) + pow(x(0) * (a_ref - Y[1](i, 0)) + x(1) * (w_ref - Y[1](i, 1)), 2);
+		matrix b(1, 2);
+		b(0, 0) = Y[1](i, 0);
+		b(0, 1) = Y[1](i, 1);
+		(*ud).set_row(b, i);
+	}
+	y = 0.1 * y;
+
+#elif LAB_NO==3 && LAB_PART==1
+	double arg = 3.14 * sqrt(pow(x(0) / 3.14, 2) + pow(x(1) / 3.14, 2));
+	y = sin(arg) / arg;
+
+	//y = pow(x(0), 2) + pow(x(1), 2);
+
+	// zewnetrzna
+	if ((*ad)(1) > 1)
+	{
+		if (-x(0) + 1 > 0) {
+			y = y + (*ad)(0) * pow(-x(0) + 1, 2);
+		}
+
+		if (-x(1) + 1 > 0) {
+			y = y + (*ad)(0) * pow(-x(1) + 1, 2);
+		}
+
+		if (sqrt(pow(x(0), 2) + pow(x(1), 2)) - (*ud)(0) > 0)
+			y = y + (*ad)(0) * pow(sqrt(pow(x(0), 2) + pow(x(1), 2)) - (*ud)(0), 2);
+	}
+	else {
+		// wewnetrzna
+		if (-x(0) + 1 > 0) {
+			y = 1e+10;
+		}
+		else {
+			y = y - (*ad)(0) / (-x(0) + 1);
+		}
+
+		if (-x(1) + 1 > 0) {
+			y = 1e+10;
+		}
+		else {
+			y = y - (*ad)(0) / (-x(1) + 1);
+		}
+
+		if (sqrt(pow(x(0), 2) + pow(x(1), 2)) - (*ud)(0) > 0) {
+			y = 1e+10;
+		}
+		else {
+			y = y - (*ad)(0) / (sqrt(pow(x(0), 2) + pow(x(1), 2)) - (*ud)(0));
+		}
+	}
+#elif LAB_NO==3 && LAB_PART==2
+
+#elif LAB_NO==4 && (LAB_PART==1 || LAB_PART==2)
+
+#elif LAB_NO==4 && LAB_PART==3
+
+#elif LAB_NO==5 && LAB_PART==1
+
+#elif LAB_NO==5 && LAB_PART==2
+
+#endif
+}
+
+void solution::grad(matrix *ud, matrix *ad)
+{
+	++g_calls;
+#if LAB_NO==4 && (LAB_PART==1 || LAB_PART==2)
+
+#elif LAB_NO==4 && LAB_PART==3
+
+#endif
+}
+
+void solution::hess(matrix *ud, matrix *ad)
+{
+	++H_calls;
+#if LAB_NO==4 && (LAB_PART==1 || LAB_PART==2)
+
+#endif
+}
